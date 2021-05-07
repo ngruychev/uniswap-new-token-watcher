587b0f3930e670a427f78337568fd48d88c3115d
Nikolay Gruychev <nickick@nickick.tk>
2021-05-07 04:56:13 +0300

    feat: improvements

---
+++ api.js
@@ -0,0 +1,79 @@
+const v2Url = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2";
+const v3Url = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3";
+
+function graphQuery(apiUrl, query, variables) {
+  return fetch(apiUrl, {
+    method: "POST",
+    body: JSON.stringify({ query, variables }),
+  }).then((res) => res.json());
+}
+
+async function paginatedPairsWithTxCount1(
+  timestamp,
+  isv3 = false,
+  minBlockNum = 0,
+) {
+  let all = [];
+  timestamp = Math.floor(timestamp);
+  while (true) {
+    const res = await graphQuery(
+      isv3 ? v3Url : v2Url,
+      `
+      query ${
+        isv3 ? "Pools" : "Pairs"
+      }WithTxCount1($timestamp: BigInt! $minBlockNum: BigInt!) {
+        ${
+        isv3 ? "pools" : "pairs"
+      }(first: 1000 orderBy: createdAtTimestamp orderDirection: desc where: { txCount: 1 createdAtBlockNumber_gt: $minBlockNum createdAtTimestamp_gt: $timestamp }) {
+          id
+          createdAtBlockNumber
+          createdAtTimestamp
+          token0Price
+          token1Price
+          token0 {
+            id
+            txCount
+            name
+            symbol
+          }
+          token1 {
+            id
+            txCount
+            name
+            symbol
+          }
+        }
+      }
+      `,
+      {
+        timestamp,
+        minBlockNum,
+      },
+    );
+    all = all.concat(res.data[isv3 ? "pools" : "pairs"]);
+    if (res.data[isv3 ? "pools" : "pairs"].length === 0) break;
+    minBlockNum = res.data[isv3 ? "pools" : "pairs"]
+      .map((p) => p.createdAtBlockNumber)
+      .map((block) => BigInt(block))
+      .reduce((a, b) => a > b ? a : b)
+      .toString();
+    if (res.data[isv3 ? "pools" : "pairs"].length < 1000) break;
+  }
+  return [all, minBlockNum];
+}
+
+export async function newTokensSince(timestamp, isv3 = false, mbn) {
+  const [pairs, minBlockNum] = await paginatedPairsWithTxCount1(
+    timestamp,
+    isv3,
+    mbn,
+  );
+  const res = pairs
+    .filter((p) => p.token0.txCount === "1" || p.token1.txCount === "1")
+    .map((pair) =>
+      pair.token0.txCount === "1"
+        ? { token: pair.token0, pair }
+        : { token: pair.token1, pair }
+    );
+  return [res, minBlockNum];
+}
--- index.js
+++ index.js
@@ -7,80 +7,86 @@ import {
 } from "https://cdn.skypack.dev/preact@10.5.13/hooks";
 import { html } from "https://cdn.skypack.dev/htm@3.0.4/preact";
 import flatpickr from "https://cdn.skypack.dev/flatpickr@4.6.9";
+import * as timeago from "https://cdn.skypack.dev/timeago.js@4.0.2";
+import { newTokensSince } from "./api.js";
 
 const appStartTime = Math.floor(Date.now() / 1000);
 
-const graphApiUrl =
-  "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2";
+const urls = {
+  token: {
+    uniswap: {
+      v2: (token) => `https://v2.info.uniswap.org/token/${token}`,
+      v3: (token) => `https://v3.info.uniswap.org/#/tokens/${token}`,
+    },
+    etherscan: (token) => `https://etherscan.io/token/${token}`,
+  },
+  pair: {
+    uniswap: {
+      v2: (pair) => `https://v2.info.uniswap.org/pair/${pair}`,
+      v3: (pair) => `https://v2.info.uniswap.org/#/pools/${pair}`,
+    },
+  },
+};
 
-function graphQuery(query, variables) {
-  return fetch(graphApiUrl, {
-    method: "POST",
-    body: JSON.stringify({ query, variables }),
-  }).then((res) => res.json());
+function TimeAgo({ timestamp, update = 1000 }) {
+  const [timeAgo, setTimeAgo] = useState("");
+  useEffect(() => {
+    const interval = setInterval(() => setTimeAgo(timeago.format(timestamp)));
+    return () => clearInterval(interval);
+  }, []);
+  return html`${timeAgo}`;
 }
 
-async function getNewTokensSince(timestamp) {
-  return await graphQuery(
-    `
-  query NewTokenQuery($timestamp: BigInt!) {
-    pairs(where: {createdAtTimestamp_gt: $timestamp}) {
-      token0 {
-        name
-        symbol
-        id
-      }
-      token1 {
-        name
-        symbol
-        id
-      }
-      id
-      createdAtTimestamp
-      createdAtBlockNumber
-      totalSupply
-      token0Price
-      token1Price
-    }
-  }`,
-    { timestamp },
-  );
-}
-
-function Pair(
+function Result(
   {
-    token0,
-    token1,
-    id,
-    createdAtTimestamp,
-    createdAtBlocknumber,
-    totalSuppy,
-    token0Price,
-    token1Price,
+    token,
+    pair,
+    uniswapVersion = "v2",
   },
 ) {
-  const { symbol: token0Symbol, name: token0Name } = token0;
-  const { symbol: token1Symbol, name: token1Name } = token1;
   return html`
-  <div class="pair">
+  <div class="result">
     <div>
-      <b>${token0Symbol} - ${token1Symbol}</b>
+      <b>${token.symbol}</b> (${token.name})
       <br/>
-      (${token0Name} / ${token1Name})
+      <b>ID:</b> ${token.id} (<a href=${
+    urls.token.uniswap[uniswapVersion](token.id)
+  }>Uniswap</a> <a href=${urls.token.etherscan(token.id)}>Etherscan</a>)
     <//>
     <br/>
-    <b>ID:</b> ${id} (<a href=${`https://info.uniswap.org/pair/${id}`}>Uniswap</a>)
+    <b>Pair:</b> ${pair.token0.symbol} / ${pair.token1.symbol} (${pair.token0.name} / ${pair.token1.name})
     <br/>
-    <b>Date:</b> ${new Date(createdAtTimestamp * 1000).toString()}
+    <b>Pair ID:</b> ${pair.id} (<a href=${
+    urls.pair.uniswap[uniswapVersion](pair.id)
+  }>Uniswap</a>)
+    <br/>
+    <b>Date:</b> ${
+    new Date(pair.createdAtTimestamp * 1000).toString()
+  } (<b><${TimeAgo} timestamp=${pair.createdAtTimestamp * 1000}/></b>)
   </div>
   `;
 }
 
-function Pairs({ pairs }) {
+function Results({ results, uniswapVersion = "v2" }) {
   return html`
-  <div class="pairs">
-    ${pairs.map((pair) => html`<${Pair} key=${pair.id} ...${pair}/>`)}
+  <div class="results">
+    ${
+    results.map((result) =>
+      html
+        `<${Result} key=${result.token.id} ...${result} uniswapVersion=${uniswapVersion}/>`
+    )
+  }
   </div>`;
+}
+
+function VersionPicker(
+  { versions = [["v2", "v2"], ["v3", "v3"]], version, onChange },
+) {
+  return html`
+  <select value=${version} onchange=${(e) => onChange(e.target.value)}>
+    ${versions.map((v) => html`<option value=${v[0]}>${v[1]}</option>`)}
+  </select>
+  `;
 }
 
 function Flatpickr({ value, onChange }) {
@@ -102,21 +108,30 @@ function Flatpickr({ value, onChange }) {
   return html`<input ref=${ref}/>`;
 }
 
-function UpdateInterval({ value, onChange }) {
-  const [multiplier, setMultiplier] = useState(1000);
-  const [val, setVal] = useState(value / multiplier);
+function UpdateInterval(
+  {
+    defaultMul = 1000,
+    multipliers = [
+      [1000, "second(s)"],
+      [1000 * 60, "minute(s)"],
+      [1000 * 60 * 60, "hour(s)"],
+    ],
+    value,
+    onChange,
+  },
+) {
+  const [mul, setMul] = useState(defaultMul);
+  const [val, setVal] = useState(value / mul);
   useEffect(() => {
-    onChange(val * multiplier);
-  }, [val, multiplier]);
+    onChange(val * mul);
+  }, [val, mul]);
   return html`
   <div class="row">
-    <input class="column" value=${val} onchange=${(e) =>
-    setVal(e.target.value)} step=${1}/>
-    <select class="column" value=${multiplier} onchange=${(e) =>
-    setMultiplier(e.target.value)}>
-      <option value=${1000}>second(s)</option>
-      <option value=${1000 * 60}>minute(s)</option>
-      <option value=${1000 * 60 * 60}>hour(s)</option>
+    <input class="column" value=${val} step=${1} min=${1} onchange=${(e) =>
+    setVal(e.target.value)}/>
+    <select class="column" value=${mul} onchange=${(e) =>
+    setMul(e.target.value)}>
+      ${multipliers.map((m) => html`<option value=${m[0]}>${m[1]}</option>`)}
     </select>
   </div>
   `;
@@ -128,20 +143,27 @@ function App(
   const [refreshInterval, setRefreshInterval] = useState(
     defaultRefreshInterval,
   );
-  const [data, setData] = useState({ pairs: [] });
+  const [uniswapVersion, setUniswapVersion] = useState("v2");
+  const [data, setData] = useState([]);
   const [since, setSince] = useState(defaultSince);
+
   async function update() {
-    const resData = await getNewTokensSince(since);
-    setData(resData.data);
+    const [resData, minBlockNum] = await newTokensSince(since, uniswapVersion === "v3", 0);
+    setData(resData);
   }
+  
   useEffect(() => {
     update();
     const interval = setInterval(update, refreshInterval);
     return () => clearInterval(interval);
-  }, [refreshInterval, since]);
+  }, [refreshInterval, since, uniswapVersion]);
+
   return html`
   <div class="app">
     <div class="row">
+      <div class="column">
+        Version: <${VersionPicker} version=${uniswapVersion} onChange=${setUniswapVersion}/>
+      </div>
       <div class="column">
         Since: <${Flatpickr} value=${since} onChange=${setSince}/>
       </div>
@@ -150,7 +172,7 @@ function App(
         <${UpdateInterval} value=${refreshInterval} onChange=${setRefreshInterval}/>
       </div>
     </div>
-    <${Pairs} pairs=${data.pairs}/>
+    <${Results} results=${data} uniswapVersion=${uniswapVersion}/>
   </div>
   `;
 }
